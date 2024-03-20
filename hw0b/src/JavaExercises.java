09866be41d3b012a01d5a5b6de86df7ab769b3bb
fanhk <fanhangkai@gmail.com>
2024-03-20 16:05:56 +0800

    hw0b javaExercises

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -5,8 +5,7 @@ public class JavaExercises {
 
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
-        // TODO: Fill in this function.
-        return null;
+        return new int[]{1, 2, 3, 4, 5};
     }
 
     /** Returns the order depending on the customer.
@@ -14,14 +13,17 @@ public class JavaExercises {
      *  If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
-        // TODO: Fill in this function.
-        return null;
+        if (customer.equals("Ergun")) {
+            return new String[]{"beyti", "pizza", "hamburger", "tea"};
+        } else if (customer.equals("Erik")) {
+            return new String[]{"sushi", "pasta", "avocado", "coffee"};
+        return new String[3];
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
-        // TODO: Fill in this function.
+
         return 0;
     }
 
